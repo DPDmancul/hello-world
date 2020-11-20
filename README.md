8f06aca5c191f892f435caaf92f3b239e2b36cd2
DPDmancul <dp.d-@live.it>
2020-11-20 14:58:14 +0100

    init

---
+++ README.md
@@ -0,0 +1,36 @@
+# Hello world!
+
+Davide Peressoni (dp.d-@live.it)
+
+An "Hello world!" which works in:
+
+  1. C
+  - C++
+  - CoffeeScript
+  - Brainf***
+  - Julia
+  - Perl
+  - PHP
+  - Python
+  - *sh (sh, bash, zsh, ...)
+  
+## Usage
+
+  1. `g++ -x c hello && ./a.out`
+  - `g++ -x c++ hello && ./a.out`
+  - `coffee hello`
+  - `./brainf.py hello`
+  - `julia hello`
+  - `perl hello`
+  - `php hello`
+  - `python3 hello`
+  - `sh hello`, `bash hello`, `zsh hello`
+
+## Automatic test
+
+```sh
+./test.sh
+```
+
+## License
+Released under MIT license
---
+++ test.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env sh
+
+F="hello"
+
+T="`mktemp`"
+
+t(){
+  R="`$2 | sed -e 'N;s/\\r$//' -e 's/^.*\\r//'`"
+  if test "$R" = "Hello world!"; then
+    echo -e "\e[32m$1: passed!\e[m"
+  else
+    echo -e "\e[31m$1: failed\e[m"
+    echo "$R"
+    # printf "%q" "$R"
+  fi
+}
+
+
+# C*
+g++ -x c -o $T $F
+t "c" "$T"
+g++ -x c++ -o $T $F
+t "c++" "$T"
+
+# COFFEESCRIPT
+t "coffeescript" "coffee $F"
+
+# BRAINF***
+t "brainf***" "./brainf.py $F"
+
+# JULIA
+t "julia" "julia $F"
+
+# PERL
+t "perl" "perl $F"
+
+# PHP
+t "php" "php $F"
+
+# PYTHON
+t "python" "python3 $F"
+
+# *SH
+t "sh" "sh $F"
+t "bash" "bash $F"
+t "zsh" "zsh $F"
+
+
+rm -f "$T"
